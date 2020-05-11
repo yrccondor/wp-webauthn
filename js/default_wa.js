518ed51f3b60ff9de7115811938b467e1b1887da
yrccondor <yrc371106@hotmail.com>
2020-05-11 23:07:06 +0800

    Update js

--- js/default_wa.js
+++ js/default_wa.js
@@ -2,15 +2,15 @@ document.addEventListener('DOMContentLoaded',function(){
     if(document.querySelectorAll("#lostpasswordform, #registerform").length > 0){
         return;
     }
-    if(!(window.PublicKeyCredential === undefined || navigator.credentials.create === undefined || typeof navigator.credentials.create !== "function")){
-        // If supported, toggle
-        wwa_dom(".user-pass-wrap,.forgetmenot,#wp-submit", (dom)=>{dom.style.display = "none"});
-        wwa_dom("wp-webauthn-notice", (dom)=>{dom.style.display = "block"}, "class");
-        wwa_dom("wp-webauthn-check", (dom)=>{dom.style.cssText = dom.style.cssText + "display: block !important"}, "id");
-        wwa_dom("user_login", (dom)=>{dom.focus()}, "id");
-        wwa_dom("wp-submit", (dom)=>{dom.disabled = true}, "id");
-    }
     window.onload = function(){
+        if(!(window.PublicKeyCredential === undefined || navigator.credentials.create === undefined || typeof navigator.credentials.create !== "function")){
+            // If supported, toggle
+            wwa_dom(".user-pass-wrap,.forgetmenot,#wp-submit", (dom)=>{dom.style.display = "none"});
+            wwa_dom("wp-webauthn-notice", (dom)=>{dom.style.display = "block"}, "class");
+            wwa_dom("wp-webauthn-check", (dom)=>{dom.style.cssText = dom.style.cssText + "display: block !important"}, "id");
+            wwa_dom("user_login", (dom)=>{dom.focus()}, "id");
+            wwa_dom("wp-submit", (dom)=>{dom.disabled = true}, "id");
+        }
         if(document.querySelectorAll("#lostpasswordform, #registerform").length > 0){
             return;
         }
