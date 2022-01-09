088ea461e2bef544f921c51b11a839a64f25de89
yrccondor <yrc371106@hotmail.com>
2022-01-09 04:07:12 +0000

    feat: add a security notice for user verification

--- js/admin.js
+++ js/admin.js
@@ -8,6 +8,20 @@ jQuery(() => {
             }, 5000);
         }
     }
+
+    jQuery('input[name=user_verification]').on('change', () => {
+        if (jQuery('input[name=user_verification]:checked').val() === 'false') {
+            jQuery('#wwa-uv-field').after(`<div class="notice notice-warning" role="alert" id="wp-webauthn-uv-warning"><p>${php_vars.i18n_1}</p></div>`);
+        } else {
+            jQuery('#wp-webauthn-uv-warning').remove();
+        }
+    });
+
+    setTimeout(() => {
+        if (jQuery('input[name=user_verification]:checked').val() === 'false') {
+            jQuery('#wwa-uv-field').after(`<div class="notice notice-warning" role="alert" id="wp-webauthn-uv-warning"><p>${php_vars.i18n_1}</p></div>`);
+        }
+    }, 0);
 })
 
 // Update log
@@ -24,27 +38,27 @@ function updateLog() {
         success: function (data) {
             if (typeof data === 'string') {
                 console.warn(data);
-                jQuery('#wwa_log').text(php_vars.i18n_8);
+                jQuery('#wwa_log').text(php_vars.i18n_3);
                 return;
             }
             if (data.length === 0) {
                 document.getElementById('clear_log').disabled = true;
                 jQuery('#wwa_log').text('');
                 jQuery('#wwa-remove-log').remove();
-                jQuery('#log-count').text(php_vars.i18n_23 + '0');
+                jQuery('#log-count').text(php_vars.i18n_2 + '0');
                 return;
             }
             document.getElementById('clear_log').disabled = false;
             let data_str = data.join('\n');
             if (data_str !== jQuery('#wwa_log').text()) {
                 jQuery('#wwa_log').text(data_str);
-                jQuery('#log-count').text(php_vars.i18n_23 + data.length);
+                jQuery('#log-count').text(php_vars.i18n_2 + data.length);
                 let div = document.getElementById('wwa_log');
                 div.scrollTop = div.scrollHeight;
             }
         },
         error: function () {
-            jQuery('#wwa_log').text(php_vars.i18n_8);
+            jQuery('#wwa_log').text(php_vars.i18n_3);
         }
     })
 }
